c056ef055172d696fe7597584ca3e4cc6a2d2b37
jahnavipasumarthi <pasumarthijahnavi8@gmail.com>
2026-08-09 08:53:05 +0530

    app.py

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def verify():
 if __name__ == "__main__":
     import os
     app.run(
-        host='0.0.0.0",
-        port=int(os.environ.get("PORT",5000)))
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT",10000)))
     )
     
